75b3bee5d84a9c92af65ed69c402defd3f084907
Max Thrift-Brothers <mhvt202@exeter.ac.uk>
2026-01-17 16:29:18 +0000

    up

--- game_functions.py
+++ game_functions.py
@@ -2,7 +2,7 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    mid = len(poss_values)/2
+    mid = len(poss_values)//2
     return poss_values[mid]
 
 # function to be used in game_2: Higher or Lower
